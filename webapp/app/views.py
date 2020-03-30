88c993329518cc3e0dc5bec1062f523db093b548
Devajji <devajji@localhost.localdomain>
2020-03-30 09:08:40 +0530

    flask skeleton

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -4,32 +4,141 @@ License: MIT
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
-from django.template import loader
-from django.http import HttpResponse
+# Python modules
+import os, logging 
 
-@login_required(login_url="/login/")
-def index(request):
-    return render(request, "index.html")
+# Flask modules
+from flask               import render_template, request, url_for, redirect, send_from_directory
+from flask_login         import login_user, logout_user, current_user, login_required
+from werkzeug.exceptions import HTTPException, NotFound, abort
 
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
+# App modules
+from app        import app, lm, db, bc
+from app.models import User
+from app.forms  import LoginForm, RegisterForm
+
+# Provide login manager with load_user callback
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Logout user
+@app.route('/logout.html')
+def logout():
+    ''' Logout user '''
+    logout_user()
+    return redirect(url_for('index'))
+
+# Reset Password - Not 
+@app.route('/reset.html')
+def reset():
+    ''' Not implemented ''' 
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/reset.html') )
+
+# Register a new user
+@app.route('/register.html', methods=['GET', 'POST'])
+def register():
+    ''' Create a new user '''
+
+    # declare the Registration Form
+    form = RegisterForm(request.form)
+
+    msg = None
+
+    if request.method == 'GET': 
+
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/register.html', form=form, msg=msg ) )
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+        email    = request.form.get('email'   , '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        # filter User out of database through username
+        user_by_email = User.query.filter_by(email=email).first()
+
+        if user or user_by_email:
+            msg = 'Error: User exists!'
+        
+        else:         
+
+            pw_hash = password #bc.generate_password_hash(password)
+
+            user = User(username, email, pw_hash)
+
+            user.save()
+
+            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
+
+    else:
+        msg = 'Input error'     
+
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/register.html', form=form, msg=msg ) )
+
+# Authenticate user
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    
+    # Declare the login form
+    form = LoginForm(request.form)
+
+    # Flask message injected into the page, in case of any errors
+    msg = None
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        if user:
+            
+            #if bc.check_password_hash(user.password, password):
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                msg = "Wrong password. Please try again."
+        else:
+            msg = "Unknown user - Please register." 
+
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/login.html', form=form, msg=msg ) )
+
+# App main route + generic routing
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path>')
+def index(path):
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    content = None
+
     try:
 
-        load_template = request.path.split('/')[-1]
-        template = loader.get_template('pages/' + load_template)
-        return HttpResponse(template.render(context, request))
-
+        # try to match the pages defined in -> pages/<input file>
+        return render_template('layouts/default.html',
+                                content=render_template( 'pages/'+path) )
     except:
+        
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/error-404.html' ) )
 
-        template = loader.get_template( 'pages/error-404.html' )
-        return HttpResponse(template.render(context, request))
-
-@login_required(login_url="/login/")
-def robot_management(request):
-    template = 'pages/robots.html'
-    return render(request, template)
+# Return sitemap 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
